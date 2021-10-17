d2eed57788547358c5525c6040febc2076860e9b
DESKTOP-DDSAMEERA <digix.sameera@hotmail.com>
2021-10-17 06:24:03 +0530

    Removed Config folder

--- config/auth-configs.js
+++
@@ -1,4 +0,0 @@
-const authConfig = {};
-authConfig.encryptorSecretKey = '393a41d556f3d8164a1520f2fb30795d';
-authConfig.jwtTokenKey = 'abc123456789010012134214252';
-module.exports = authConfig;
--- config/server-config.js
+++
@@ -1,3 +0,0 @@
-var serverConfigs= {}
-serverConfigs.port = 3000;
-module.exports = serverConfigs;
