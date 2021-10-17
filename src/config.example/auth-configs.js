2d1b29540f2e0404b73713644663b43205fcc1ac
DESKTOP-DDSAMEERA <digix.sameera@hotmail.com>
2021-10-17 12:31:59 +0530

    added 02 minutes for JWT Token

--- src/config.example/auth-configs.js
+++ src/config.example/auth-configs.js
@@ -5,5 +5,5 @@
 const authConfig = {};
 authConfig.encryptorSecretKey = '393a41d556f3d8164a1520f2fb30795d'; //Encryptor Secret Key
 authConfig.jwtTokenKey = 'abc123456789010012134214252'; // JWT Token Key
-authConfig.jwtTokenExpireTime = 180; // Default 180 : 30 minutes (60x30 )
+authConfig.jwtTokenExpireTime = 120; // Default 120 : 2 minutes (60x2 )
 module.exports = authConfig; // JWT Token Expiration Time , Default : 30 minutes
